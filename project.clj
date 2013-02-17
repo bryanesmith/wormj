af29411e78bd2d6cf09a58d9717f2d3fb86ba381
Bryan Smith <bryanesmith@gmail.com>
2013-02-16 21:58:44 -0500

    Bumped to version 0.2.0 (since API available), and filled out some proj info.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject wormj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject wormj "0.2.0"
+  :description "Clojure port of BSD game `worm` - the growing worm game."
+  :url "https://github.com/bryanesmith/wormj"
+  ; TODO: Determine license. 
+  ; :license {:name ""
+  ;           :url ""}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojure-lanterna "0.9.2"]])
