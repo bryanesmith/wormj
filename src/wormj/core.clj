47dcc315a5a15876ed418a5041205f42217190c2
Bryan Smith <bryanesmith@gmail.com>
2013-02-17 11:55:16 -0500

    Can draw game, though not efficiently. Controls not available yet, either.

--- src/wormj/core.clj
+++ src/wormj/core.clj
@@ -1,6 +1,8 @@
 (ns wormj.core
-  (:require wormj.functions
-            wormj.state))
+  (:require [wormj.functions   :as f]
+            [wormj.state       :as s]
+            [lanterna.terminal :as t])
+  (:gen-class))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  
 (defn r-map-key
@@ -19,8 +21,8 @@
          head      (peek pos)
          head-key  (r-map-key head)
          body      (pop pos)]
-  (into {apple-key apple-val head-key "@" } 
-        (map #(vector (r-map-key %) "o") body))))
+  (into {apple-key (char (+ 48 apple-val)) head-key \@ } 
+        (map #(vector (r-map-key %) \o) body))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
 (defn render-wall
@@ -55,4 +57,89 @@
         (for [y (range 0 height)]
           (str "*" (render-row y width rmap) "*"))
         (render-wall :bottom width)))))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn print-game-over-msg
+  ""
+  []
+  (println)
+  (println "Well, you ran into something and the game is over.")
+  (println "Your final score was" (f/score @s/worm))
+  (println))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+; TODO: the type of terminal should be specified by user.
+(def term (t/get-terminal :unix))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn draw-board
+  "Draw the board to the terminal"
+  []
+  (let [board-width  (:x (:size @s/board))
+        board-height (:y (:size @s/board))]
+
+    ; "Worm"
+    (t/move-cursor term 1 0)
+    (t/put-string term "Wormj")
+
+    ; Top wall
+    (t/move-cursor term 0 1)
+    (t/put-string term (render-wall :top board-width))
+
+    ; Side walls
+    (loop [i 0]
+      (when (< i board-height)
+        (t/move-cursor term 0 (+ 2 i))
+        (t/put-string term "*")
+        (t/move-cursor term (+ board-width 1) (+ 2 i) )
+        (t/put-string term "*")
+        (recur (inc i))))
+
+    ; Bottom wall
+    (t/move-cursor term 0 (+ board-height 2))
+    (t/put-string term (render-wall :bottom board-width))))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn draw-updates
+  "Updates screen: worm, apple, score"
+  []
+  (let [b-width  (:x (:size @s/board))
+        b-height (:y (:size @s/board))
+        rmap   (r-map @s/worm @s/board)]
+    (doseq [x (range 0 b-width) 
+            y (range 0 b-height)]
+      (let [rkey (r-map-key {:x x :y y})]
+        (t/move-cursor term (+ x 1) (+ y 2))
+        (t/put-character term (rkey rmap \space ))))))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn run
+  "Handles execution game once terminal is stated"
+  []
+  (let [term-size (t/get-size term)
+        board-x   (- (first term-size) 2)
+        board-y   (- (last term-size)  3)]
+    (wormj.state/init-game board-x board-y)
+    (draw-board)
+    (loop [] ; Loop until game over
+      (if-not (f/game-over? @s/worm @s/board)
+        (do
+          (draw-updates)         
+          (. Thread (sleep 50))
+          (s/advance-turn)
+          (recur))))))
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn -main
+  "User runs game."
+  [& args]
+  (try 
+    (do
+      (t/start term)
+      (run))
+  (catch Exception e
+    (println "An error occured: " (.getMessage e)))
+  (finally 
+    (do
+      (t/stop term)
+      (print-game-over-msg)))))
 
