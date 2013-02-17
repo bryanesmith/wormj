a08263c558527c51571940188847d5ca560b1e72
Bryan Smith <bryanesmith@gmail.com>
2013-02-17 13:23:10 -0500

    Rendering performance optimizations: only draw changes (7x speed up)

--- src/wormj/core.clj
+++ src/wormj/core.clj
@@ -5,27 +5,27 @@
   (:gen-class))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  
-(defn r-map-key
+(defn to-str-map-key
   "Given a point, creates key."
   [point]
   (keyword (str (:x point) "_" (:y point))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-(defn r-map
-  "Returns a map of maps with character data. (x_y (render-game-map))"
+(defn to-str-map
+  "Returns a map of maps with character data. (x_y (game-to-stto-str-map))"
   [worm board]
   (let [ apple     (:apple board)
-         apple-key (r-map-key (:position apple)) 
+         apple-key (to-str-map-key (:position apple)) 
          apple-val (:val apple)
          pos       (:position worm)
          head      (peek pos)
-         head-key  (r-map-key head)
+         head-key  (to-str-map-key head)
          body      (pop pos)]
   (into {apple-key (char (+ 48 apple-val)) head-key \@ } 
-        (map #(vector (r-map-key %) \o) body))))
+        (map #(vector (to-str-map-key %) \o) body))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-(defn render-wall
+(defn wall-to-str
   "Renders top or bottom wall to string"
   [type width]
   (case type
@@ -34,29 +34,29 @@
     (throw (Exception. (str "Invalid wall type: " type)))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-(defn render-row
-  "Renders a row on the board to string"
-  [y width rmap]  
+(defn row-to-str
+  "Renders a row of the board  to string"
+  [y width str-map]  
   (apply str
     (for [x (range 0 width)]
-      (let [key (r-map-key {:x x :y y})]
-        (key rmap " ")))))
+      (let [key (to-str-map-key {:x x :y y})]
+        (key str-map " ")))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-(defn render-game
+(defn game-to-str
   "Render current game state as collection of strings"
   []
   (let [board  @wormj.state/board
         worm   @wormj.state/worm
         width  (:x (:size board))
         height (:y (:size board))
-        rmap   (r-map worm board)]
+        str-map   (to-str-map worm board)]
     (flatten
       (vector
-        (render-wall :top width)  
+        (wall-to-str :top width)  
         (for [y (range 0 height)]
-          (str "*" (render-row y width rmap) "*"))
-        (render-wall :bottom width)))))
+          (str "*" (row-to-str y width str-map) "*"))
+        (wall-to-str :bottom width)))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
 (defn print-game-over-msg
@@ -84,7 +84,7 @@
 
     ; Top wall
     (t/move-cursor term 0 1)
-    (t/put-string term (render-wall :top board-width))
+    (t/put-string term (wall-to-str :top board-width))
 
     ; Side walls
     (loop [i 0]
@@ -97,23 +97,73 @@
 
     ; Bottom wall
     (t/move-cursor term 0 (+ board-height 2))
-    (t/put-string term (render-wall :bottom board-width))))
+    (t/put-string term (wall-to-str :bottom board-width))))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+; Source: http://goo.gl/Teu2N 
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn subtract-lists[a b]
+   (remove (into #{} b) a))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(def prev-apple (ref nil))
+(def prev-worm  (ref nil))
+(def board-char (char \space))
+
+; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+(defn draw-board-character
+  "Draw character at specified board (not screen) coord."
+  [c coord]
+  (t/move-cursor term (+ 1 (:x coord)) (+ 2 (:y coord)))
+  (t/put-character term c))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
 (defn draw-updates
   "Updates screen: worm, apple, score"
   []
-  (let [b-width  (:x (:size @s/board))
-        b-height (:y (:size @s/board))
-        rmap   (r-map @s/worm @s/board)]
-    (doseq [x (range 0 b-width) 
-            y (range 0 b-height)]
-      (let [rkey (r-map-key {:x x :y y})]
-        (t/move-cursor term (+ x 1) (+ y 2))
-        (t/put-character term (rkey rmap \space ))))))
+
+  ; If prev-worm set, overwrite any positions that
+  ; are no longer part of worm.
+  (if-not (nil? @prev-worm)
+    (let [old-w (:position @prev-worm)
+          new-w (:position @s/worm)]
+    (doseq [seg (subtract-lists old-w new-w)]
+      (draw-board-character board-char seg))))
+
+  ; Render worm body
+  (if (nil? @prev-worm)
+
+    ; No prev-worm, so draw entire worm
+    (doseq [seg (pop (:position @s/worm))]
+      (draw-board-character \o seg))
+
+    ; Prev-worm, just draw difference
+    (let [old-w (pop (:position @prev-worm))
+          new-w (pop (:position @s/worm))]
+      (doseq [seg (subtract-lists new-w old-w)]
+        (draw-board-character \o seg))))
+
+  ; Render worm head
+  (draw-board-character \@ (peek (:position @s/worm)))
+
+  ; Render apple (if necessary)
+  (if (or (nil? @prev-apple)
+          (not= @prev-apple (:apple @s/board)))
+    (let [apple (:apple @s/board)
+          val   (:val   apple)
+          pos   (:position apple)]
+      (draw-board-character (char (+ 48 val)) pos)))
+
+  ; Move cursor out of way
+  (t/move-cursor term 999 999)
+  
+  ; Store ref to worm
+  (dosync
+    (ref-set prev-worm  @s/worm)
+    (ref-set prev-apple (:apple @s/board))))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-(defn run
+(defn run-in-term
   "Handles execution game once terminal is stated"
   []
   (let [term-size (t/get-size term)
@@ -125,9 +175,10 @@
       (if-not (f/game-over? @s/worm @s/board)
         (do
           (draw-updates)         
-          (. Thread (sleep 50))
+          (. Thread (sleep 100))
           (s/advance-turn)
           (recur))))))
+
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
 (defn -main
   "User runs game."
@@ -135,7 +186,7 @@
   (try 
     (do
       (t/start term)
-      (run))
+      (run-in-term))
   (catch Exception e
     (println "An error occured: " (.getMessage e)))
   (finally 
