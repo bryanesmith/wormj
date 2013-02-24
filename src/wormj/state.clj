7eab9cbae9e447d223fa1cfc44c8deb76f589687
Bryan Smith <bryanesmith@gmail.com>
2013-02-24 11:58:55 -0500

    Using 'require as' in state (for more readable namespacing)

--- src/wormj/state.clj
+++ src/wormj/state.clj
@@ -1,5 +1,5 @@
 (ns wormj.state
-  (:require wormj.functions))
+  (:require [wormj.functions :as f]))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
 (def init-worm-len 8)         ; Initial worm length is 8
@@ -11,7 +11,7 @@
 (defn set-trajectory
   "Set trajectory"
   [way]
-  {:pre [(wormj.functions/valid-direction? way)]}
+  {:pre [(f/valid-direction? way)]}
     (dosync (ref-set trajectory way)))
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
@@ -22,7 +22,7 @@
     (let [val (int (+ 1 (rand-int 9)))
           new-x (rand-int max-x)
           new-y (rand-int max-y)
-          apple (wormj.functions/build-apple val new-x new-y)]
+          apple (f/build-apple val new-x new-y)]
       (if-not (some #(= (:position apple) %) (:position worm))
         apple
         (recur)))))
@@ -31,9 +31,9 @@
 (defn advance-turn
   "Advance one turn: using trajectory, move worm, potentially consume/generate apple."
   []
-  (let [moved-worm (wormj.functions/move-worm @worm @trajectory)]
+  (let [moved-worm (f/move-worm @worm @trajectory)]
     (dosync(ref-set worm moved-worm))
-    (if (wormj.functions/apple-consumed? @board @worm)
+    (if (f/apple-consumed? @board @worm)
       ; Apple consumed - modify, then set, new worm to have growth count & generate/set new apple
       (let [growing-worm (assoc moved-worm :grow-count (:val (:apple @board)))]
         (dosync
@@ -45,8 +45,8 @@
   "Initialize the game."
   [b-width b-height]
   (set-trajectory :right)
-  (let [worm# (wormj.functions/gen-worm 0 (quot b-height 2) init-worm-len)]
+  (let [worm# (f/gen-worm 0 (quot b-height 2) init-worm-len)]
     (dosync
       (ref-set worm worm#)
-      (ref-set board (wormj.functions/gen-board (gen-apple b-width b-height worm#) b-width b-height)))))
+      (ref-set board (f/gen-board (gen-apple b-width b-height worm#) b-width b-height)))))
 
